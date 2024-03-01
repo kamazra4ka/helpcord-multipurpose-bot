9e347e69ba32b17131883edbfe71d60cfa246360
KamazRa4ka <thera4okml@gmail.com>
2024-03-01 13:55:21 +0100

    Started styling temp voice channels messages + renamed them to "Lounges"

--- src/bot.js
+++ src/bot.js
@@ -59,6 +59,8 @@ client.on('ready', async () => {
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isModalSubmit()) return;
 
+    let embed;
+
     if (interaction.customId === 'setupTempVoicesModal') {
         // Get the data entered by the user
         const channelName = interaction.fields.getTextInputValue('channelName');
@@ -111,10 +113,38 @@ client.on(Events.InteractionCreate, async interaction => {
                 options: options
             });
 
-            await interaction.reply({ content: `blah blah blah`, ephemeral: true });
+            embed = new EmbedBuilder()
+                .setColor('#041c3c')
+                .setTitle('Helpcord | Lounge')
+                .setDescription(`**${interaction.user.username}**, you have successfully set up lounges for your server. Members can now join the main channel and create their own lounges.`)
+                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
+                .setTimestamp()
+                .setFooter({
+                    text: 'Helpcord | Multipurpose bot for Discord',
+                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
+                });
+
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: false
+            });
         } catch (error) {
             console.error('Failed to create category or channel:', error);
-            await interaction.reply({ content: 'Failed to create category or channel. Please check the bot permissions and try again.', ephemeral: true });
+            embed = new EmbedBuilder()
+                .setColor('#041c3c')
+                .setTitle('Helpcord | Lounge')
+                .setDescription(`**${interaction.user.username}**, an error occurred while creating the category or channel. Please check bot's permissions and try again.`)
+                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
+                .setTimestamp()
+                .setFooter({
+                    text: 'Helpcord | Multipurpose bot for Discord',
+                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
+                });
+
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
         }
 
     }
