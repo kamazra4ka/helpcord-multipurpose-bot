18dc54ab4632b978f4033a0c744dc1da271262a3
KamazRa4ka <thera4okml@gmail.com>
2024-03-01 19:24:26 +0100

    Moved out a big chunk of code out of bot.js + fixed a bug with loungesetup command

--- src/bot.js
+++ src/bot.js
@@ -32,6 +32,7 @@ const db = new QuickDB();
 import {showModal} from "./Handlers/TempVoices/showModalTempVoices.js";
 import {SetupTempVoices} from "./Commands/TempVoices/SetupTempVoices.js";
 import {newTempChannelCreation} from "./Handlers/TempVoices/newTempChannelCreation.js";
+import {tempVoiceModalHandler} from "./Handlers/TempVoices/tempVoiceModalHandler.js";
 const tempChannels = new TempChannels(client);
 
 
@@ -65,137 +66,8 @@ tempChannels.on("childCreate", async (member, channel, parentChannel) => {
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isModalSubmit()) return;
 
-    let embed;
-
     if (interaction.customId === 'setupTempVoicesModal') {
-        // Get the data entered by the user
-        const channelName = interaction.fields.getTextInputValue('channelName');
-        const categoryName = interaction.fields.getTextInputValue('categoryName');
-        let maxMembers = 3;
-        try {
-            maxMembers = parseInt(interaction.fields.getTextInputValue('maxMembers'));
-        } catch (e) {
-            embed = new EmbedBuilder()
-                .setColor('#041c3c')
-                .setTitle('Helpcord | Lounge')
-                .setDescription(`**${interaction.user.username}**, an error occurred while creating the category or channel. Max members must be a number.`)
-                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
-                .setTimestamp()
-                .setFooter({
-                    text: 'Helpcord | Multipurpose bot for Discord',
-                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
-                });
-
-            await interaction.reply({
-                embeds: [embed],
-                ephemeral: true
-            });
-        }
-
-        if (isNaN(maxMembers)) {
-            embed = new EmbedBuilder()
-                .setColor('#041c3c')
-                .setTitle('Helpcord | Lounge')
-                .setDescription(`**${interaction.user.username}**, an error occurred while creating the category or channel. Max members must be a number.`)
-                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
-                .setTimestamp()
-                .setFooter({
-                    text: 'Helpcord | Multipurpose bot for Discord',
-                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
-                });
-
-            await interaction.reply({
-                embeds: [embed],
-                ephemeral: true
-            });
-            return;
-        }
-
-        if (!categoryName.trim() || !channelName.trim()) {
-
-            embed = new EmbedBuilder()
-                .setColor('#041c3c')
-                .setTitle('Helpcord | Lounge')
-                .setDescription(`**${interaction.user.username}**, an error occurred while creating the category or channel. Names cannot be empty.`)
-                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
-                .setTimestamp()
-                .setFooter({
-                    text: 'Helpcord | Multipurpose bot for Discord',
-                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
-                });
-
-            await interaction.reply({
-                embeds: [embed],
-                ephemeral: true
-            });
-            return;
-        }
-
-        try {
-            const category = await interaction.guild.channels.create({
-                name: categoryName,
-                type: ChannelType.GuildCategory,
-            });
-
-            const mainChannel = await interaction.guild.channels.create({
-                name: channelName,
-                type: ChannelType.GuildVoice,
-                parent: category.id,
-            });
-
-            await tempChannels.registerChannel(mainChannel.id, {
-                childCategory: category.id,
-                childAutoDeleteIfEmpty: true,
-                childMaxUsers: maxMembers,
-                childFormat: (member, count) => `#${count} | ${member.user.username}'s lounge`
-            });
-
-            let options = {
-                childCategory: category.id,
-                childAutoDeleteIfEmpty: true,
-                childMaxUsers: maxMembers,
-                childFormat: (member, count) => `#${count} | ${member.user.username}'s lounge`
-            }
-
-            await db.push("temp-channels", {
-                channelID: mainChannel.id,
-                options: options
-            });
-
-            embed = new EmbedBuilder()
-                .setColor('#041c3c')
-                .setTitle('Helpcord | Lounge')
-                .setDescription(`**${interaction.user.username}**, you have successfully set up lounges for your server. Members can now join the main channel and create their own lounges.`)
-                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
-                .setTimestamp()
-                .setFooter({
-                    text: 'Helpcord | Multipurpose bot for Discord',
-                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
-                });
-
-            await interaction.reply({
-                embeds: [embed],
-                ephemeral: false
-            });
-        } catch (error) {
-            console.error('Failed to create category or channel:', error);
-            embed = new EmbedBuilder()
-                .setColor('#041c3c')
-                .setTitle('Helpcord | Lounge')
-                .setDescription(`**${interaction.user.username}**, an error occurred while creating the category or channel. Please check bot's permissions and try again.`)
-                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213106150569152512/lounges.png?ex=65f44424&is=65e1cf24&hm=11e6f2b230a5c1ffdf4f389461fd4959a181a1e613caec8fce40a2804975cf8d&=&format=webp&quality=lossless&width=1440&height=391')
-                .setTimestamp()
-                .setFooter({
-                    text: 'Helpcord | Multipurpose bot for Discord',
-                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
-                });
-
-            await interaction.reply({
-                embeds: [embed],
-                ephemeral: true
-            });
-        }
-
+        await tempVoiceModalHandler(interaction, tempChannels, db)
     }
 });
 
@@ -215,7 +87,7 @@ client.on('interactionCreate', async interaction => {
             case 'slowmode':
                 await SlowmodeTextChannel(interaction);
                 break;
-            case 'voicesetup':
+            case 'loungesetup':
                 await SetupTempVoices(interaction);
                 break;
             default:
