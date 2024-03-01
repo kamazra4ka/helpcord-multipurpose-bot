d15f85d022989eb5315b599d8daf4972decbb0a2
KamazRa4ka <thera4okml@gmail.com>
2024-03-01 11:34:58 +0100

    Added a different message for removing a slowmode

--- src/Commands/Moderation/Slowmode.js
+++ src/Commands/Moderation/Slowmode.js
@@ -64,16 +64,31 @@ export const Slowmode = async (interaction) => {
     try {
         await channel.setRateLimitPerUser(time, `Slowmode set by ${interaction.user.tag}`);
 
-        embed = new EmbedBuilder()
-            .setColor('#041c3c')
-            .setTitle('Helpcord | Slowmode')
-            .setDescription(`**${interaction.user.username}** has set the slowmode for ${channel} to ${time} seconds.`)
-            .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213069333241274368/slowmode.png?ex=65f421da&is=65e1acda&hm=ad202b710a8ff2aebb81f5024766e0b9bf71a317f27fed5bc2504460a3ba8831&=&format=webp&quality=lossless&width=1440&height=391')
-            .setTimestamp()
-            .setFooter({
-                text: 'Helpcord | Multipurpose bot for Discord',
-                iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
-            });
+        if (time === 0) {
+            embed = new EmbedBuilder()
+                .setColor('#041c3c')
+                .setTitle('Helpcord | Slowmode')
+                .setDescription(`**${interaction.user.username}** has removed slowmode for ${channel}.`)
+                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213069333241274368/slowmode.png?ex=65f421da&is=65e1acda&hm=ad202b710a8ff2aebb81f5024766e0b9bf71a317f27fed5bc2504460a3ba8831&=&format=webp&quality=lossless&width=1440&height=391')
+                .setTimestamp()
+                .setFooter({
+                    text: 'Helpcord | Multipurpose bot for Discord',
+                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
+                });
+        } else {
+            embed = new EmbedBuilder()
+                .setColor('#041c3c')
+                .setTitle('Helpcord | Slowmode')
+                .setDescription(`**${interaction.user.username}** has set the slowmode for ${channel} to ${time} seconds.`)
+                .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213069333241274368/slowmode.png?ex=65f421da&is=65e1acda&hm=ad202b710a8ff2aebb81f5024766e0b9bf71a317f27fed5bc2504460a3ba8831&=&format=webp&quality=lossless&width=1440&height=391')
+                .setTimestamp()
+                .setFooter({
+                    text: 'Helpcord | Multipurpose bot for Discord',
+                    iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
+                });
+        }
+
+
 
         await interaction.reply({
             embeds: [embed],
