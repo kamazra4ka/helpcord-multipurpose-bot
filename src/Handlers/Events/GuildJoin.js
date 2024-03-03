25e5c5306164b80c797e10dd03790ecd746c82bd
KamazRa4ka <thera4okml@gmail.com>
2024-03-03 12:58:37 +0100

    Added an error handler to prevent bot from crashing

--- src/Handlers/Events/GuildJoin.js
+++ src/Handlers/Events/GuildJoin.js
@@ -48,11 +48,14 @@ export const guildJoin = async (guild) => {
             iconURL: 'https://media.discordapp.net/attachments/1212377559669669930/1213033923853029396/7206b41c-5f05-47dc-87cd-d6433649b201.png?ex=65f400e0&is=65e18be0&hm=330ee0af24766736475e0627ab2c87cbfa42055ff2b25bde191a21269a349930&=&format=webp&quality=lossless&width=487&height=487'
         });
 
-    await systemChannel.send({
-        content: '',
-        components: [row, row2],
-        embeds: [startEmbed]
-    });
-
+    try {
+        await systemChannel.send({
+            content: '',
+            components: [row, row2],
+            embeds: [startEmbed]
+        });
+    } catch (error) {
+        console.error('Failed to send message:', error);
+    }
 
 }
