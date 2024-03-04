efb7d35567f9808b6da27d40b8455b0731275a5f
KamazRa4ka <thera4okml@gmail.com>
2024-03-04 09:36:53 +0100

    Fixed one more bug with the Branding command

--- src/Commands/Customization/Branding.js
+++ src/Commands/Customization/Branding.js
@@ -5,9 +5,9 @@ export const Branding = async (interaction) => {
     await turnBranding(interaction.guildId);
 
     const startEmbed = new EmbedBuilder()
-        .setColor(await getBranding(interaction.guildId))
+        .setColor(await getEmbed(interaction.guildId))
         .setTitle('Helpcord | Branding')
-        .setDescription(`**${interaction.user.username}**, the branding for the bot's embeds on this server has been turned ${await getEmbed(interaction.guildId) ? '**Off**' : '**On**'}.`)
+        .setDescription(`**${interaction.user.username}**, the branding for the bot's embeds on this server has been turned ${await getBranding(interaction.guildId) ? '**Off**' : '**On**'}.`)
         .setImage('https://media.discordapp.net/attachments/1212377559669669930/1214127739729350666/branding.png?ex=65f7fb92&is=65e58692&hm=397d525ce2a68e53a5917daf2afd38f18d1a5012add12c714d401ed002456c5e&=&format=webp&quality=lossless&width=1586&height=431')
         .setTimestamp()
         .setFooter({
