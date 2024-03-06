850fb1e625ddd98a376009cb3c2e80e93fe4189e
KamazRa4ka <thera4okml@gmail.com>
2024-03-06 15:30:45 +0100

    Implemented new permission check system in lock & unlock commands

--- src/Commands/Moderation/UnlockTextChannel.js
+++ src/Commands/Moderation/UnlockTextChannel.js
@@ -1,18 +1,19 @@
 import {PermissionFlagsBits, ChannelType, EmbedBuilder} from 'discord.js';
 import {getEmbed} from "../../Handlers/Database/Customization.js";
 import {getFooterDetails} from "../../Handlers/getFooterDetails.js";
+import {checkPermissions} from "../../Handlers/Permissions.js";
 
 export const UnlockTextChannel = async (interaction) => {
+
     const footer = await getFooterDetails(interaction);
     let channel = interaction.options.getChannel('channel') || interaction.channel, embed;
 
-    // if the user doesn't have the required permissions to unlock the channel
-    if (!interaction.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
+    if (!await checkPermissions(interaction, 'MANAGE_CHANNELS')) {
         embed = new EmbedBuilder()
             .setColor(await getEmbed(interaction.guildId))
-            .setTitle('Helpcord | Channel lockdown')
-            .setDescription(`**${interaction.user.username}**, you don't have the required permissions to unlock this channel.`)
-            .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213061264021127168/lock.png?ex=65f41a56&is=65e1a556&hm=df13deaa51df5a2dc0143185ae8dc59dda99f87f483c6f4560ef8fc4e1b11600&=&format=webp&quality=lossless&width=1440&height=391')
+            .setTitle('Helpcord | Permissions')
+            .setDescription(`**${interaction.user.username}**, you don't have the required permissions to continue with this action.`)
+            .setImage('https://media.discordapp.net/attachments/1212377559669669930/1214942980490665994/permissions.png?ex=65faf2d3&is=65e87dd3&hm=f678dd79a9ea3f583e703b43a40acd14286b248e20958c3bbbb3625df39217ec&=&format=webp&quality=lossless&width=1921&height=521')
             .setTimestamp()
             .setFooter({
                 text: `${footer.footerText}`,
