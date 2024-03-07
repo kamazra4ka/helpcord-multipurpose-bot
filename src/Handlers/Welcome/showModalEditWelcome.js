b30c0d079e2da08c7b8fde9d6bd11e3f674e15b1
KamazRa4ka <thera4okml@gmail.com>
2024-03-07 11:39:12 +0100

    Added a function to edit custom message in welcome cards

--- src/Handlers/Welcome/showModalEditWelcome.js
+++ src/Handlers/Welcome/showModalEditWelcome.js
@@ -49,3 +49,47 @@ export const ModalEditImage = async (interaction) => {
 
         await interaction.showModal(modal);
 };
+
+export const ModalEditMessage = async (interaction) => {
+
+    const footer = await getFooterDetails(interaction);
+
+    if (!await checkPermissions(interaction, 'MANAGE_GUILD')) {
+        const embed = new EmbedBuilder()
+            .setColor(await getEmbed(interaction.guildId))
+            .setTitle('Helpcord | Permissions')
+            .setDescription(`**${interaction.user.username}**, you don't have the required permissions to continue with this action.`)
+            .setImage('https://media.discordapp.net/attachments/1212377559669669930/1214942980490665994/permissions.png?ex=65faf2d3&is=65e87dd3&hm=f678dd79a9ea3f583e703b43a40acd14286b248e20958c3bbbb3625df39217ec&=&format=webp&quality=lossless&width=1921&height=521')
+            .setTimestamp()
+            .setFooter({
+                text: `${footer.footerText}`,
+                iconURL: `${footer.footerIcon}`
+            });
+
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        });
+        return;
+    }
+
+    const modal = new ModalBuilder()
+        .setCustomId('editMessageModal')
+        .setTitle('Edit message');
+
+    const categoryNameInput = new TextInputBuilder()
+        .setCustomId('channelMessage')
+        .setLabel('Message')
+        .setStyle(TextInputStyle.Paragraph)
+        .setMinLength(1)
+        .setMaxLength(2000)
+        .setValue('**\$count** | Hey, **\$user**! Welcome to **\$server**! We hope you enjoy your stay!')
+        .setPlaceholder('Use \$count for the member count, \$user for the user\'s name and \$server for the server name.')
+        .setRequired(true);
+
+    const secondActionRow = new ActionRowBuilder().addComponents(categoryNameInput);
+
+    modal.addComponents(secondActionRow);
+
+    await interaction.showModal(modal);
+};
