d63b4afc08a6bd3875f6d6840724dfdfcf6b4dc0
KamazRa4ka <thera4okml@gmail.com>
2024-03-07 11:35:58 +0100

    Added a modal for changing the bg image for the welcome card

---
+++ src/Handlers/Welcome/showModalEditWelcome.js
@@ -0,0 +1,51 @@
+import {
+    ActionRowBuilder,
+    TextInputStyle,
+    ModalBuilder, TextInputBuilder, EmbedBuilder
+} from 'discord.js';
+import {checkPermissions} from "../Permissions.js";
+import {getEmbed} from "../Database/Customization.js";
+import {getFooterDetails} from "../getFooterDetails.js";
+
+export const ModalEditImage = async (interaction) => {
+
+    const footer = await getFooterDetails(interaction);
+
+    if (!await checkPermissions(interaction, 'MANAGE_GUILD')) {
+        const embed = new EmbedBuilder()
+            .setColor(await getEmbed(interaction.guildId))
+            .setTitle('Helpcord | Permissions')
+            .setDescription(`**${interaction.user.username}**, you don't have the required permissions to continue with this action.`)
+            .setImage('https://media.discordapp.net/attachments/1212377559669669930/1214942980490665994/permissions.png?ex=65faf2d3&is=65e87dd3&hm=f678dd79a9ea3f583e703b43a40acd14286b248e20958c3bbbb3625df39217ec&=&format=webp&quality=lossless&width=1921&height=521')
+            .setTimestamp()
+            .setFooter({
+                text: `${footer.footerText}`,
+                iconURL: `${footer.footerIcon}`
+            });
+
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        });
+        return;
+    }
+
+        const modal = new ModalBuilder()
+            .setCustomId('editImageModal')
+            .setTitle('Edit image');
+
+        const channelImage = new TextInputBuilder()
+            .setCustomId('channelImage')
+            .setLabel('Welcome image background URL')
+            .setStyle(TextInputStyle.Short)
+            .setMinLength(1)
+            .setMaxLength(512)
+            .setPlaceholder('Leave empty to use a default image')
+            .setRequired(false);
+
+        const secondActionRow = new ActionRowBuilder().addComponents(channelImage);
+
+        modal.addComponents(secondActionRow);
+
+        await interaction.showModal(modal);
+};
