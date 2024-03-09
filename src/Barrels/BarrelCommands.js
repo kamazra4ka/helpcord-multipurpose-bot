fe300768f7df39fc9dd45ee818dfa4f7982e20de
KamazRa4ka <thera4okml@gmail.com>
2024-03-09 23:43:24 +0100

    Optimized imports (added barrel files for managing them)

---
+++ src/Barrels/BarrelCommands.js
@@ -0,0 +1,11 @@
+import { LockTextChannel } from "../Commands/Moderation/Channel/LockTextChannel.js";
+import { UnlockTextChannel } from "../Commands/Moderation/Channel/UnlockTextChannel.js";
+import { SlowmodeTextChannel } from "../Commands/Moderation/Channel/SlowmodeTextChannel.js";
+import { SetupTempVoices } from "../Commands/TempVoices/SetupTempVoices.js";
+import { Help } from "../Commands/Help.js";
+import { SetColor } from "../Commands/Customization/setColor.js";
+import { Branding } from "../Commands/Customization/Branding.js";
+import { welcomeChannelCreate } from "../Commands/Welcome/welcomeChannelCreate.js";
+import { TempVoicesBan } from "../Commands/Moderation/User/TempVoicesBan.js";
+
+export { LockTextChannel, UnlockTextChannel, SlowmodeTextChannel, SetupTempVoices, Help, SetColor, Branding, welcomeChannelCreate, TempVoicesBan };
