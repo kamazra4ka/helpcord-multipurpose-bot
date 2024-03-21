85b42666a40e4f894e27a6b5b333e10dce869128
KamazRa4ka <thera4okml@gmail.com>
2024-03-21 20:05:47 +0100

    Removed /adminprogess command

--- src/Handlers/HelpButtons.js
+++ src/Handlers/HelpButtons.js
@@ -55,7 +55,7 @@ export const HelpButtons = async (interaction, category) => {
             embed = new EmbedBuilder()
                 .setColor(await getEmbed(interaction.guildId))
                 .setTitle('Helpcord | Server commands')
-                .setDescription('Hey! Helpcord is a multipurpose bot that can help you with a variety of tasks. Moderation, fun, economy and much more!\n\n`/adminprogress` - Check your server\'s progress in implementing bot\'s features and receive tips.\n`/loungesetup` - Enable "Lounges" on your server. Members will be able to create their own lounges and then talk there.\n`/setcolor` - Set the color of the embeds for the bot on this server.')
+                .setDescription('Hey! Helpcord is a multipurpose bot that can help you with a variety of tasks. Moderation, fun, economy and much more!\n\n`/loungesetup` - Enable "Lounges" on your server. Members will be able to create their own lounges and then talk there.\n`/setcolor` - Set the color of the embeds for the bot on this server.')
                 .setImage('https://media.discordapp.net/attachments/1212377559669669930/1213043629602639872/help.png?ex=65f409ea&is=65e194ea&hm=354028e7a08cc45e8657c5f3ecb80b0d5eca5d56243e6ffa0d357b2944961bce&=&format=webp&quality=lossless&width=687&height=186')
                 .setTimestamp()
                 .setFooter({
